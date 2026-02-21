1bf447c1e7beeb996a54c1e6fab104cb126fdfa4
H-Kalariya <hetvi.kalariya@gmail.com>
2026-02-21 14:57:51 +0530

    final

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,27 +1,5 @@
-// Firebase is disabled in favor of Manual Authentication (LocalStorage)
-// to prevent "client is offline" errors during the development session.
 
-/*
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCCY49PPM1AM33HcPYPAa19OuY3q2q0k9c",
-    authDomain: "oddo-f5a27.firebaseapp.com",
-    projectId: "oddo-f5a27",
-    storageBucket: "oddo-f5a27.firebasestorage.app",
-    messagingSenderId: "286400679674",
-    appId: "1:286400679674:web:c1905352b9cc1401248343",
-    measurementId: "G-XYW0Z6KBFV"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-*/
-
-// Exporting null/dummies to avoid breaking imports, though service files should be updated
 export const auth = null as any;
 export const db = null as any;
 
